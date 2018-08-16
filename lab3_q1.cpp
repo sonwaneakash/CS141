30c2fbeab17ddc3bcf0a44139238aa6c7960919e
sonwaneakash <42178604+sonwaneakash@users.noreply.github.com>
2018-08-16 21:14:35 +0530

    Add files via upload

---
+++ lab3_q1.cpp
@@ -0,0 +1,27 @@
+// operating with variables
+ 
+#include <iostream>
+using namespace std;
+int main()
+{
+// declaring variables:
+int a=5;
+ 
+char b='1';
+ 
+float c=1.02;
+ 
+double d=2.087;
+ 
+bool e=6;
+ 
+// shows size
+cout << "size of integer " << sizeof(int) << " bytes "<<" a = "<<a<<endl;
+cout << "size of char " << sizeof(char) << " bytes "<<" b = "<<b<<endl;
+cout << "size of integer " << sizeof(float) << " bytes "<<" c = "<<c<<endl;
+cout << "size of integer " << sizeof(double) << " bytes "<<" d = "<<d<<endl;
+cout << "size of integer " << sizeof(bool) << " bytes "<<" e = "<<e<<endl;
+return 0;
+}
+
+
---
+++ lab3_q2.cpp
@@ -0,0 +1,38 @@
+// operating with variables
+ 
+#include <iostream>
+using namespace std;
+int main()
+{
+// declaring variables:
+int a;
+int b;
+int r1;
+//  mathematical process:
+a = 462;
+b = 390;
+r1 = a + b;
+cout << r1 <<endl;
+int c,d,r2;
+c=85;
+d=92;
+r2 = a - b;
+cout << r2 <<endl;
+int e,f,r3;
+e=87;
+f=25;
+r3=a * b;
+cout << r3 <<endl;
+float g,h,r4;
+g=9;
+h=2;
+r4=g/h;
+cout << r4 <<endl;
+int i,j,r5;
+i=10;
+j=6;
+r5=i%j;
+cout << r5 <<endl;
+// Ends the program:
+return 0;
+}
